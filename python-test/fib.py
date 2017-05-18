56a362b29a8a903ff75d162b36ac5e2f48c58880
Hannes Rosenkranz <Frodo@Hannes-MacBook-Pro.local>
2017-05-18 14:41:54 +0200

    tried some python

---
+++ python-test/fib.py
@@ -0,0 +1,6 @@
+#Die Summe der letzten beiden Elemente ergibt das naechste
+a = 0;
+b = 1;
+while b < 10:
+    print(b)
+    a, b = b, a+b
---
+++ python-test/ifTrueMountainDew.py
@@ -0,0 +1,17 @@
+x = int(input("Please enter an integer!"))
+
+if x < 0:
+    x = 0;
+    print('Negative changed to zero dude')
+
+elif x==0:
+    print ('zero')
+
+elif x == 1:
+    print('almost')
+
+elif x == 2:
+    print('MountainDew!')
+
+else:
+    print('less')
